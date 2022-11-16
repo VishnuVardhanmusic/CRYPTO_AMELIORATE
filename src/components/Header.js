f5b7a4823ebd72c1550baaf717473e9b36c87987
VishnuVardhanmusic <68817474+VishnuVardhanmusic@users.noreply.github.com>
2022-11-16 21:15:49 +0530

    commit

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,7 +5,7 @@ function Header() {
     <nav className="navbar navbar-expand-md fixed-top bg-light ">
       <div className="container ">
         <h1  className=" text-center text-dark font-weight-bold " >
-        CRYPTO <span class="text-info">fdasa</span>
+        CRYPTO <span class="text-info">Ameliorate</span>
         </h1>
           </div>
     </nav>
